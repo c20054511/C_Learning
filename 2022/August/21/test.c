1eb80ef194be57e27a71380a8ec4c2a1dc9d8c18
changxr233 <3056096467@qq.com>
2022-08-21 22:09:05 +0800

    test_0821

--- 2022/August/21/test.c
+++ 2022/August/21/test.c
@@ -8,7 +8,7 @@
 } */
 
 // 输入两个数字相加后打印
-/* int main()
+int main()
 {
     int num1 = 0;
     int num2 = 0;
@@ -17,4 +17,4 @@
     sum = num1 + num2;
     printf("%d\n",sum);
     return 0;
-} */
+} 
