9e140c729e13b9b080cfb74fc7f7c888af40b96b
changxr233 <3056096467@qq.com>
2022-08-21 22:02:32 +0800

    C_Learning_2022

---
+++ 2022/August/21/test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// 打印文本
+/* int main()
+{
+    printf("Hello World");
+    return 0;
+} */
+
+// 输入两个数字相加后打印
+/* int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    scanf("%d %d",&num1,&num2 );
+    int sum = 0;
+    sum = num1 + num2;
+    printf("%d\n",sum);
+    return 0;
+} */
