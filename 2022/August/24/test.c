e75e3c78e2081dc74069040df2e5752089390d54
changxr233 <3056096467@qq.com>
2022-08-24 22:12:05 +0800

    test0824

---
+++ 2022/August/24/test.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+//转译字符
+/* int main()
+{
+    printf("%c\n",'\130');  //将十进制转换成八进制，输出ASCII码表中的值
+    printf("%c\n",'\x42');  //将十进制转换成十六进制，输出ASCII码表中的值
+    return 0;
+} */
+
+//注释
+//int main()
+//{
+    //双斜杠用于注释单行
+    //int a = 0;    
+    
+    // “/* */”用于注释多行   
+    /* int b = 0;
+    int c = 0;
+    int d = 0;     
+    int e = 0;
+    int f = 0; */
+    //return 0;
+//}
+
+
+//if 判断语句
+/* int main()
+{
+    int input = 0;
+    printf("你要好好学习吗(1/0)\n");
+    scanf("%d",&input); //获取输入内容
+    if (input == 1)
+    {
+        printf("赢取白富美\n");
+    }
+    else
+    {
+        printf("进厂\n");
+    }
+    return 0;
+} */
